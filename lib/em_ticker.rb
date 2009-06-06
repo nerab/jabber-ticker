8127e9a18664f137e5c8f492f74fcb0a0a6b6f05
nerab <suhlig@.(none)>
2009-06-07 00:04:15 +0200

    moved ideas to TODO

--- lib/em_ticker.rb
+++ lib/em_ticker.rb
@@ -21,16 +21,3 @@ cfg = YAML.load_file('config/em_ticker.yml')
 ActiveRecord::Base.establish_connection(cfg[:db])
 Thread.abort_on_exception = true
 TickerBot.new(cfg[:bot], TickerListLoader.new(cfg[:ticker][:url])).connect
-
-# TODO There are more tickers. Let's scrape them too and make them available.
-# TickerBot.new(TickerRegistry.new('http://sport.ard.de/sp/portrait/ticker_termine.jsp')).connect
-#
-# A DTM Ticker is different
-# url = 'http://sport.ard.de/sp/layout/php/sportticker/?event_art=24&ticker=819&event=3287'
-# doc = Hpricot.parse(Iconv.iconv('UTF-8//IGNORE', 'ISO-8859-1', open(url).read).join)
-# doc.search('//div[@id='tickerMeldungen']/p').each{|line|
-#     runde = line.search('span').inner_text
-# 	line.search('span').remove
-#     puts '-> #{runde}' if !runde.empty?
-#     puts line.inner_text
-# }
