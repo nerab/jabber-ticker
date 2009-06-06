de36e8353bafbe288ae8ca18037e6f0cbbea22f8
nerab <suhlig@.(none)>
2009-06-06 23:19:32 +0200

    Fixed formatter back to Ruby defaults

--- lib/em_ticker.rb
+++ lib/em_ticker.rb
@@ -14,6 +14,8 @@ require 'em_ticker/score'
 
 LOG = Logger.new('log/development.log')
 LOG.level = Logger::DEBUG
+# ActiveRecord required ActiveSupport, and this one changes the formatter to SimpleFormatter (which we don't want)
+LOG.formatter = Logger::Formatter.new
 
 cfg = YAML.load_file('config/em_ticker.yml')
 ActiveRecord::Base.establish_connection(cfg[:db])
